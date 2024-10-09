a456a110f28c082034271c8ec8193799cab3c6c7
kravi2201 <avinashbth10@gmail.com>
2024-10-10 00:27:02 +0530

    File updated

--- Cats and a Mouse.cpp
+++ Cats and a Mouse.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<stdlib.h>
 
 using namespace std;
 
@@ -27,59 +30,16 @@ return mouse;
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    int q;
-    cin >> q;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    for (int q_itr = 0; q_itr < q; q_itr++) {
-        string xyz_temp;
-        getline(cin, xyz_temp);
-
-        vector<string> xyz = split_string(xyz_temp);
-
-        int x = stoi(xyz[0]);
-
-        int y = stoi(xyz[1]);
-
-        int z = stoi(xyz[2]);
-
-        string result = catAndMouse(x, y, z);
-
-        fout << result << "\n";
+    int no_of_test_case;
+    int catA;
+    int catB;
+    int mouse;
+    cin>>no_of_test_case;
+    while(no_of_test_case--){
+        cin>>catA;
+        cin>>catB;
+        cin>>mouse;
+        cout<<catAndMouse(catA,catB,mouse)<<endl;
     }
-
-    fout.close();
-
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
